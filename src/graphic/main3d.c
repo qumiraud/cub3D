dbcd2d32ac1a34173d4bae299e734105b7c1c909
Pierre-Louis Jurdana <pjurdana@2A6.42angouleme.fr>
2025-07-31 11:20:42 +0200

    window is actually cleaning itself

--- src/graphic/main3d.c
+++ src/graphic/main3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main3d.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qumiraud <qumiraud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pjurdana <pjurdana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 16:29:43 by qumiraud          #+#    #+#             */
-/*   Updated: 2025/07/30 15:58:39 by qumiraud         ###   ########.fr       */
+/*   Updated: 2025/07/31 11:18:44 by pjurdana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,67 +47,67 @@
 // }
 void    fill_texture_tab(t_data *data)
 {
-    int width = TEXWIDTH;
-    int height = TEXHEIGHT;
-    int size_line, endian;
+	int width = TEXWIDTH;
+	int height = TEXHEIGHT;
+	int size_line, endian;
 
-    data->texture_img[0] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->NO, &width, &height);
-    data->texture_img[1] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->SO, &width, &height);
-    data->texture_img[2] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->EA, &width, &height);
-    data->texture_img[3] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->WE, &width, &height);
+	data->texture_img[0] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->NO, &width, &height);
+	data->texture_img[1] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->SO, &width, &height);
+	data->texture_img[2] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->EA, &width, &height);
+	data->texture_img[3] = mlx_xpm_file_to_image(data->win->mlx_ptr, data->WE, &width, &height);
 
-    for (int i = 0; i < 4; i++)
-    {
-        if (!data->texture_img[i])
-        {
-            fprintf(stderr, "Erreur lors du chargement de la texture %d\n", i);
-            exit(1);
-        }
-        data->texture[i] = (int *)mlx_get_data_addr(data->texture_img[i], &data->bpp, &size_line, &endian);
-        if (!data->texture[i])
-        {
-            fprintf(stderr, "mlx_get_data_addr failed for texture %d\n", i);
-            exit(1);
-        }
-    }
+	for (int i = 0; i < 4; i++)
+	{
+		if (!data->texture_img[i])
+		{
+			fprintf(stderr, "Erreur lors du chargement de la texture %d\n", i);
+			exit(1);
+		}
+		data->texture[i] = (int *)mlx_get_data_addr(data->texture_img[i], &data->bpp, &size_line, &endian);
+		if (!data->texture[i])
+		{
+			fprintf(stderr, "mlx_get_data_addr failed for texture %d\n", i);
+			exit(1);
+		}
+	}
 }
 
 void    copy_buffer_to_image(t_data *data, __uint32_t buffer[SCREEN_HEIGHT][SCREEN_WIDTH])
 {
 	mlx_clear_window(data->win->mlx_ptr, data->win->mlx_win);
 	for (int x = 0; x < SCREEN_WIDTH; x++)
-    {
+	{
 		for (int y = 0; y < SCREEN_HEIGHT; y++)
-        {
-            int pixel_index = y * data->size_line + x * (data->bpp / 8);
-            if (pixel_index < 0 || pixel_index >= SCREEN_HEIGHT * data->size_line)
-            {
-                fprintf(stderr, " pixel_index hors limites: %d\n", pixel_index);
-                exit(1);
-            }
-            *((unsigned int *)(data->img_data + pixel_index)) = buffer[y][x];
+		{
+			int pixel_index = y * data->size_line + x * (data->bpp / 8);
+			if (pixel_index < 0 || pixel_index >= SCREEN_HEIGHT * data->size_line)
+			{
+				fprintf(stderr, " pixel_index hors limites: %d\n", pixel_index);
+				exit(1);
+			}
+			*((unsigned int *)(data->img_data + pixel_index)) = buffer[y][x];
 
 		}
-    }
+	}
 	mlx_put_image_to_window(data->win->mlx_ptr, data->win->mlx_win, data->img_ptr, 0, 0);
 
-    // mlx_put_image_to_window(data->win->mlx_ptr, data->win->mlx_win, data->img_ptr, 0, 0);
+	// mlx_put_image_to_window(data->win->mlx_ptr, data->win->mlx_win, data->img_ptr, 0, 0);
 }
 
-void    init_image(t_data *data)
+void	init_image(t_data *data)
 {
-    data->img_ptr = mlx_new_image(data->win->mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT);
-    if (!data->img_ptr)
-    {
-        fprintf(stderr, "mlx_new_image failed\n");
-        exit(1);
-    }
-    data->img_data = mlx_get_data_addr(data->img_ptr, &data->bpp, &data->size_line, &data->endian);
-    if (!data->img_data)
-    {
-        fprintf(stderr, "mlx_get_data_addr for main image failed\n");
-        exit(1);
-    }
+	data->img_ptr = mlx_new_image(data->win->mlx_ptr, SCREEN_WIDTH, SCREEN_HEIGHT);
+	if (!data->img_ptr)
+	{
+		fprintf(stderr, "mlx_new_image failed\n");
+		exit(1);
+	}
+	data->img_data = mlx_get_data_addr(data->img_ptr, &data->bpp, &data->size_line, &data->endian);
+	if (!data->img_data)
+	{
+		fprintf(stderr, "mlx_get_data_addr for main image failed\n");
+		exit(1);
+	}
 }
 
 void	dir_player(t_data *data, char c)
@@ -206,6 +206,20 @@ void	raycasting_loop(t_data *data)
 	x = 0;
 	side_dist_x = 0;
 	side_dist_y = 0;
+	
+	for (int y = 0; y < SCREEN_HEIGHT; y++)
+	{
+		for (int x = 0; x < SCREEN_WIDTH; x++)
+		{
+			if (y < SCREEN_HEIGHT / 2)
+				buffer[y][x] = 0x333366; // couleur du ciel
+			else
+				buffer[y][x] = 0x222222; // couleur du sol
+		}
+	}
+
+
+	
 	while (x < SCREEN_WIDTH)
 	{
 		camera_x = 2.00 * (double)x / SCREEN_WIDTH - 1;
@@ -343,9 +357,9 @@ void	raycasting_loop(t_data *data)
 	data->bigben->old_time = data->bigben->time;
 	data->bigben->time = clock();
 	data->bigben->frame_time = (data->bigben->time - data->bigben->old_time) / CLOCKS_PER_SEC;
-	data->player->move_speed = data->bigben->frame_time * 5.0;
+	data->player->move_speed = data->bigben->frame_time * 50.0;
 	printf ("FPS : %.2f\n", 1 / data->bigben->frame_time);
-	data->player->rot_speed = data->bigben->frame_time * 3.0;
+	data->player->rot_speed = data->bigben->frame_time * 30.0;
 }
 
 int main(int argc, char **argv)
